1684366bba2f27454e33e79f6bb2d20caade2148
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-07 22:10:33 -0500

    answered ReadMe questions copied into index.js

--- src/index.js
+++ src/index.js
@@ -27,3 +27,17 @@ ReactDOM.render(
 //1. Add in all necessary components and library methods.
 //2. Create a store that includes thunk middleware support.
 //3. Wrap the App component in a react-redux Provider element.
+
+// READ ME QUESTIONS:
+
+// What problem does the context API help solve?
+// Context API helps to make prop drilling less necessary by helping the state to be more globally accesible. 
+// In your own words, describe actions, reducers and the store and their role in Redux.
+// Actions are the ACTION that is dispatched after an event and are stored in a reducer. They allow predictability for state.
+// What does each piece do? Why is the store known as a 'single source of truth' in a redux application? A store is known as a single source of truth because it holds all of the state and it is ummuttable, only changed by actions.
+// What is the difference between Application state and Component state? When would be a good time to use one over the other?
+//Application state is available to the entire application, for instance the need for Login and Password or UI would be a good use case, whereas component is only available to a specific component.
+// Describe redux-thunk, what does it allow us to do? How does it change our action-creators?
+// Redux thunk allows us to run our code asychronously in Redux, whereas usually you cannot. It is like a the middleman talking to the actions through thunk.
+// What is your favorite state management system you've learned and this sprint? Please explain why!
+// I like being able to hold all of the state in a reducer and using actions with redux.
